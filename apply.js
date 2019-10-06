02cdd941657d37dd3814a67ea7d3d29f694744ab
GALT-A <55669755+GALT-A@users.noreply.github.com>
2019-10-06 14:29:06 +0300

    Update apply.js

--- apply.js
+++ apply.js
@@ -12,20 +12,16 @@ const apply = (count, f, arg) => {
 console.log(apply(2, Math.sqrt, 81));   // 3
 console.log(apply(5, v => v + 10, 3));  // 53
  
-console.log('*****************************************************');
+console.log('*');
+
+export default apply;
+
 
 
 
 
 const apply = (num, func, res) => {
   return (num === 0) ? res : apply (num-1, func, func(res));
-} 
- 
-//apply(1,2,3);
+}                                                                   //apply(1,2,3);
  
 apply(2,x => x*x,2)
-
-
-
-
-// 
