364318934990e6207c069dbf341159849848ab9a
GALT-A <55669755+GALT-A@users.noreply.github.com>
2019-10-06 14:08:54 +0300

    Create isPalindromme.js

---
+++ isPalindromme.js
@@ -0,0 +1,18 @@
+const length = str => str.length; // задаём длину строки
+const substr = (str, start, size) => str.substr(start, size); // возыращвем подстроку
+const isPalindrome = (string) => {
+  if (length(string) <= 1) {
+    return true;                    // если строка меньше 2 символов, то true
+  }
+  const firstSymbol = string[0];     // первый символ
+  const lastSymbol = string[length(string) - 1];   // последний символ
+ 
+  if (firstSymbol !== lastSymbol) {    // первый и последний символы одинаковые, то ок
+    return false;
+  }
+ 
+  const stringWithoutFirstAndLastSymbols = substr(string, 1, length(string) - 2);
+ 
+  return isPalindrome(stringWithoutFirstAndLastSymbols);
+};
+isPalindrome('radar');
